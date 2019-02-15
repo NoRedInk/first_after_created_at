10ddca69417a2225c333aac989ea38f985ed8c19
Michael Glass <me@mike.is>
2019-02-15 16:58:43 +0100

    Support rails 5.2

--- test/dummy/db/migrate/20170802143942_create_has_timestamps.rb
+++ test/dummy/db/migrate/20170802143942_create_has_timestamps.rb
@@ -1,4 +1,4 @@
-class CreateHasTimestamps < ActiveRecord::Migration
+class CreateHasTimestamps < ActiveRecord::Migration[5.2]
   def change
     create_table :has_timestamps do |t|
 
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802143942) do
+ActiveRecord::Schema.define(version: 2017_08_02_143942) do
 
   create_table "has_timestamps", force: :cascade do |t|
     t.datetime "created_at", null: false
